bf148f0c0d931e05f2da37c705a10e7d8d2e0339
DeivRicardoSS <deivysonrssantos@outlook.com>
2025-02-21 10:57:55 -0300

    atualizando ignite-feed

--- ignite_feed/src/App.jsx
+++ ignite_feed/src/App.jsx
@@ -5,6 +5,7 @@ import { Button } from './components/Button';
 import { Header } from './components/Header';
 import styles from "./App.module.css";
 import { SideBar } from './components/SideBar';
+import { Post } from './components/Post';
 
 export function App() {
   const [count, setCount] = useState(0)
@@ -14,8 +15,10 @@ export function App() {
       <Header/>
       <div className={styles.wrapper}>
         <SideBar/>
-        <main>
-
+        <main className={styles.posts}>
+          <Post/>
+          <Post/>
+          <Post/>
         </main>
       </div>
 
--- ignite_feed/src/components/SideBar/index.jsx
+++ ignite_feed/src/components/SideBar/index.jsx
@@ -5,7 +5,8 @@ export function SideBar() {
         <aside className={styles.container}>
             <img className={styles.cover} src="https://images.unsplash.com/photo-1518057111178-44a106bad636?q=80&w=988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
             <div className={styles.profile}>
-                <strong>Nome</strong>
+                <img src="https://avatars.githubusercontent.com/u/131066052?v=4" alt="profile" />
+                <strong>Deivyson Santos</strong>
                 <span>Web Developer</span>
             </div>
             <footer>
